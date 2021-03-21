89ff40e8a45e73a021af23e579b392397f4a0c1a
Abdul <abdullahmd326@gmail.com>
2021-03-20 22:41:37 -0400

    Adding styles and colours to granularized components

--- react-overhaul-undercontruction/src/Components/Chess/Chess.jsx
+++ react-overhaul-undercontruction/src/Components/Chess/Chess.jsx
@@ -6,7 +6,7 @@ const Chess = () => {
             <div className="chess-section">
                 <h1 className="chess-heading">Chess</h1>
                 <p className="chess-paragraph-first">A beautiful, mind-challenging game. Chess has always been an instant classic. From risky gambits to total domination, Chess really makes you think outside-the-box. It's one of my favourite games to play during my spare time. 
-                    Below are a few games I've compiled over the years. For more information about my Chess Profile, <a href="somewhere">Click Here.</a></p>
+                    Below are a few games I've compiled over the years. For more information about my Chess Profile, <a style={{color: "green"}} href="somewhere">click here.</a></p>
                 <div className="upper-row row">
                     <div className="chess-row col-lg-6 col-md-12 col-sm-12">
                         <h5 className="chess-title">Knight's Signature: Smothered Mate</h5>
